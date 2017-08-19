082157d60677a1f456bdb16910258a76be91d124
Meira Pentermann <meira@meirapentermann.com>
2017-08-19 15:27:34 -0600

    Hand class getters and setters

--- src/com/meirapentermann/blackjack/Hand.java
+++ src/com/meirapentermann/blackjack/Hand.java
@@ -1,5 +1,26 @@
 package com.meirapentermann.blackjack;
 
+import java.util.List;
+
+import com.meirapentermann.cards.Card;
+
 public class Hand {
+	private List<Card> hand;
+	
+	public Hand() {
+	}
+
+	public Hand(List<Card> hand) {
+		this();
+		this.hand = hand;
+	}
+
+	public List<Card> getHand() {
+		return hand;
+	}
+
+	public void setHand(List<Card> hand) {
+		this.hand = hand;
+	}
 
 }
