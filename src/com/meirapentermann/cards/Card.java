c7363be771420d95c7ea2d5332874b1099d6eca5
Meira Pentermann <meira@meirapentermann.com>
2017-08-19 16:21:05 -0600

    Modifying Card toString output

--- src/com/meirapentermann/cards/Card.java
+++ src/com/meirapentermann/cards/Card.java
@@ -32,44 +32,26 @@ public class Card {
 	public String toString() {
 		StringBuilder sb = new StringBuilder("");
 		sb.append("[ ");
-		sb.append(printChars(rank));
-		sb.append("  ");
+		sb.append(suit.displayChar);
+		sb.append(" ");
+		sb.append(rank.displayShort);
+		sb.append(" ");
+		sb.append(suit.displayChar);
+		sb.append(" ");
+		sb.append(" ");
+		sb.append(" ");
+		sb.append(" ");
 		sb.append(rank.displayText);
 		sb.append(suit.displayText);
-		sb.append("  ");
-		sb.append(printChars(rank));
+		sb.append(" ");
+		sb.append(" ");
+		sb.append(suit.displayChar);
+		sb.append(" ");
+		sb.append(rank.displayShort);
+		sb.append(" ");
+		sb.append(suit.displayChar);
 		sb.append(" ]\n");
 		return sb.toString();
 	}
-	
-	public String printChars(Rank rank) {
-		StringBuilder sb = new StringBuilder("");
-		switch(rank) {
-		case ACE:
-			sb.append(suit.displayChar);
-			sb.append(" A ");
-			sb.append(suit.displayChar);
-			break;
-		case JACK:
-			sb.append(suit.displayChar);
-			sb.append(" J ");
-			sb.append(suit.displayChar);
-			break;
-		case QUEEN:
-			sb.append(suit.displayChar);
-			sb.append(" Q ");
-			sb.append(suit.displayChar);
-			break;
-		case KING:
-			sb.append(suit.displayChar);
-			sb.append(" K ");
-			sb.append(suit.displayChar);
-			break;
-		default:
-			for(int i = 0; i< rank.value; i++) {
-				sb.append(suit.displayChar);
-			}
-		}
-		return sb.toString();
-	}
+
 }
--- src/com/meirapentermann/cards/Rank.java
+++ src/com/meirapentermann/cards/Rank.java
@@ -1,25 +1,27 @@
 package com.meirapentermann.cards;
 
 public enum Rank {
-	  ACE("Ace of ", 11)
-	, TWO("Two of ", 2)
-	, THREE("Three of ", 3)
-	, FOUR("Four of ", 4)
-	, FIVE("Five of ", 5)
-	, SIX("Six of ", 6)
-	, SEVEN("Seven of ", 7)
-	, EIGHT("Eight of ", 8)
-	, NINE("Nine of ", 9)
-	, TEN("Ten of ", 10)
-	, JACK("Jack of ", 10)
-	, QUEEN("Queen of ", 10)
-	, KING("King of ", 10);
+	  ACE("Ace of ", "A", 11)
+	, TWO("Two of ", "2", 2)
+	, THREE("Three of ", "3", 3)
+	, FOUR("Four of ", "4", 4)
+	, FIVE("Five of ", "5", 5)
+	, SIX("Six of ", "6", 6)
+	, SEVEN("Seven of ", "7", 7)
+	, EIGHT("Eight of ", "8", 8)
+	, NINE("Nine of ", "9", 9)
+	, TEN("Ten of ", "10", 10)
+	, JACK("Jack of ", "J", 10)
+	, QUEEN("Queen of ", "Q", 10)
+	, KING("King of ", "K", 10);
 	
 	public String displayText;
+	public String displayShort;
 	public int value;
 	
-	private Rank (String s, int v) {
+	private Rank (String s, String c, int v) {
 		this.displayText = s;
+		this.displayShort = c;
 		this.value = v;
 	}
 
@@ -29,6 +31,14 @@ public enum Rank {
 
 	public void setDisplayText(String displayText) {
 		this.displayText = displayText;
+	}
+	
+	public String getDisplayShort() {
+		return displayShort;
+	}
+
+	public void setDisplayShort(String displayShort) {
+		this.displayShort = displayShort;
 	}
 
 	public int getValue() {
