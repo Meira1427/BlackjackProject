297858a6d3543e934a5e561aa89f1812359650f1
Meira Pentermann <meira@meirapentermann.com>
2017-08-19 15:48:43 -0600

    changes to Hand class

--- src/com/meirapentermann/blackjack/Hand.java
+++ src/com/meirapentermann/blackjack/Hand.java
@@ -1,5 +1,6 @@
 package com.meirapentermann.blackjack;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.meirapentermann.cards.Card;
@@ -8,11 +9,16 @@ public class Hand {
 	private List<Card> hand;
 	
 	public Hand() {
+		this.hand = new ArrayList<>();
 	}
 
 	public Hand(List<Card> hand) {
 		this();
 		this.hand = hand;
+	}
+	
+	public void addCard(Card c) {
+		this.hand.add(c);
 	}
 
 	public List<Card> getHand() {
@@ -22,5 +28,20 @@ public class Hand {
 	public void setHand(List<Card> hand) {
 		this.hand = hand;
 	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("");
+		sb.append("* * * * * * * * * * * * * *\n\n");
+		List<Card> cards = this.getHand();
+		for(Card card : cards) {
+			sb.append(card);
+			sb.append("\n");
+		}
+		sb.append("* * * * * * * * * * * * * *\n");
+		return sb.toString();
+	}
+	
+	
 
 }
--- test/HandTests.java
+++ test/HandTests.java
@@ -1,12 +1,45 @@
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
-public class HandTests {
+import com.meirapentermann.blackjack.Hand;
+import com.meirapentermann.cards.Card;
+import com.meirapentermann.cards.Deck;
 
+public class HandTests {
+	Deck d;
+	Hand h;
+	Card c1, c2, c3;
+
+	@Before
+	public void setUp() throws Exception {
+		d = new Deck();
+		d.shuffleDeck();
+		c1 = d.dealCard();
+		c2 = d.dealCard();
+		c3 = d.dealCard();
+		h = new Hand();
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		d = null;
+		h = null;
+		c1 = null;
+		c2 = null;
+		c3 = null;
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_adding_cards_to_hand() {
+		h.addCard(c1);
+		h.addCard(c2);
+		System.out.println(h);
+		assertEquals(2, h.getHand().size());
+		
 	}
 
 }
